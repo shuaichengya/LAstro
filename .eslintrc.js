b8687dd63172f0d42640ee55fa2f676a2744cc30
chengyaxiong <1281277038@qq.com>
2023-03-07 00:09:02 +0800

    feat: 代码提交husky,tainwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,36 +4,36 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:astro/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
   ],
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ['*.astro'],
+      files: ["*.astro"],
       // Enable this plugin
-      plugins: ['astro'],
+      plugins: ["astro"],
       env: {
         // Enables global variables available in Astro components.
         node: true,
-        'astro/astro': true,
+        "astro/astro": true,
         es2020: true,
       },
       // Allows Astro components to be parsed.
-      parser: 'astro-eslint-parser',
+      parser: "astro-eslint-parser",
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
         // The script of Astro components uses ESM.
-        sourceType: 'module',
+        sourceType: "module",
       },
       rules: {
         // Enable recommended rules
-        'astro/no-conflict-set-directives': 'error',
-        'astro/no-unused-define-vars-in-style': 'error',
+        "astro/no-conflict-set-directives": "error",
+        "astro/no-unused-define-vars-in-style": "error",
 
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"
@@ -42,13 +42,13 @@ module.exports = {
     {
       // Define the configuration for `<script>` tag.
       // Script in `<script>` is assigned a virtual file name with the `.js` extension.
-      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
       env: {
         browser: true,
         es2020: true,
       },
       parserOptions: {
-        sourceType: 'module',
+        sourceType: "module",
       },
       rules: {
         // override/add rules settings here, such as:
@@ -56,15 +56,15 @@ module.exports = {
 
         // If you are using "prettier/prettier" rule,
         // you don't need to format inside <script> as it will be formatted as a `.astro` file.
-        'prettier/prettier': 'off',
+        "prettier/prettier": "off",
       },
     },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint"],
   rules: {},
 };
